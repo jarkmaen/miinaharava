713b2f4a77c31ef6ab970e684ec16e8b9bc9286b
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2021-03-30 20:26:50 +0300

    week 2 exercises

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -15,6 +15,39 @@ public class MaksukorttiTest {
 
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertTrue(kortti != null);
+    }
+
+    @Test
+    public void konstruktoriAsettaaSaldonOikein() {
+        assertEquals(10, kortti.saldo(), 0.01);
+    }
+
+    @Test
+    public void kortilleVoiLadataRahaa() {
+        kortti.lataaRahaa(10);
+        assertEquals("saldo: 0.20", kortti.toString());
+    }
+
+    @Test
+    public void kortiltaVoiOttaaRahaa() {
+        kortti.otaRahaa(5);
+        assertEquals("saldo: 0.5", kortti.toString());
+    }
+
+    @Test
+    public void rahanOttaminenEiVieSaldoaNegatiiviseksi() {
+        kortti.otaRahaa(100);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+
+    @Test
+    public void palauttaaTrueJosRahatRiittävät() {
+        assertEquals(true, kortti.otaRahaa(10));
+    }
+
+    @Test
+    public void palauttaaFalseJosRahatEivätRiitä() {
+        assertEquals(false, kortti.otaRahaa(11));
     }
 }
